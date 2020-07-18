6cebb2b11056839af7a4bd1fb758e76d17f9127c
KumarSChandra <58375738+KumarSChandra@users.noreply.github.com>
2020-07-18 15:14:11 -0700

    maybe doing stuff?

---
+++ src/ClientHandler.java
@@ -0,0 +1,10 @@
+/**should recieve:
+    array list of buttons pressed consisting of (w,a,s,d,up,down,left,right,)
+    mouse location
+
+ **should send
+    an object containing all of the the aspects of the map(including the player)
+
+ */
+public class ClientHandler {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-public class Main
-{
-  public static void main(String[] args)
-  {
-    System.out.println("We Should Do Things");
-  }
+//should create client handlers whenever someone connects
+public class Main {
+    public static void main(String[] args){
+        System.out.println("urmom");
+    }
 }
