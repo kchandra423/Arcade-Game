4341c79719a8d2621b654391954541d2019f5a18
Nathaniel-github <55164518+Nathaniel-github@users.noreply.github.com>
2020-07-27 14:08:52 -0700

    Started building map code
    Started to build the code for the Map and Obstacle classes. Also structured how rendering will work.

--- src/Map.java
+++ src/Map.java
@@ -1,20 +1,64 @@
 import java.awt.Color;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Map implements Serializable {
 
 	private static final long serialVersionUID = -3247302376139655273L;
-
+	
+	private final int MAPWIDTH = 4200;
+	private final int MAPHEIGHT = 2400;
+	
+	private final int CHUNKWIDTH = 200;
+	
+	private HashMap<Integer, ArrayList<Obstacle>> dataMap = new HashMap<Integer, ArrayList<Obstacle>>();
+	
+	public Map(String fileName) {
+		
+		
+		
+	}
+	
 	public Color getBackgroundColor() {
 		
 		return null;
 		
 	}
 	
-	public ArrayList<Obstacle> getAllObstacles(){
+	public int getMapWidth() {
 		
-		return null;
+		return MAPWIDTH;
+		
+	}
+	
+	public int getMapHeight() {
+		
+		return MAPHEIGHT;
+		
+	}
+	
+	public ArrayList<Obstacle> getChunkData(int chunkColumn, int chunkRow) {
+		
+		if (dataMap.containsKey(chunkColumn * chunkRow)) {
+			
+			return dataMap.get(chunkColumn * chunkRow);
+			
+		} else {
+			
+			return getNullChunk(chunkColumn, chunkRow);
+			
+		}
+		
+	}
+	
+	private ArrayList<Obstacle> getNullChunk(int chunkColumn, int chunkRow) {
+		
+		ArrayList<Obstacle> temp = new ArrayList<Obstacle>();
+		
+		temp.add(new Obstacle(chunkColumn * CHUNKWIDTH - CHUNKWIDTH / 2, chunkRow * CHUNKWIDTH - CHUNKWIDTH / 2, "NullBlock"));
+		
+		return temp;
 		
 	}
 	
--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,6 +1,26 @@
 
-public interface Obstacle {
+public class Obstacle {
 	
+	private final int X;
+	private final int Y;
 	
+	public Obstacle(int xCord, int yCord, String imagePathName) {
+		
+		X = xCord;
+		Y = yCord;
+		
+	}
+	
+	public int getX() {
+		
+		return X;
+		
+	}
+	
+	public int getY() {
+		
+		return Y;
+		
+	}
 
 }
